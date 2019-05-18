7bd02ec8f596786f61a8569f1bd0d89b4bae8f3b
jrodr087 <jrodr087@ucr.edu>
2019-05-18 01:34:33 -0700

    adds ability to upload recipe to DB

--- src/src/RecipeUpload.tsx
+++ src/src/RecipeUpload.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { generate } from "shortid";
 import TextForm, { title } from './title.js';
 import DescriptionForm, { description } from './description.js';
+import firebase from './firebase.js';
 
 interface IngredientList {
   id: string;
@@ -13,15 +14,58 @@ interface StepList {
   id: string;
   Step: string;
 }
-
 const UploadRecipe = () => {
+  var errorMessage = "";
   const [IngredientList, setIngredientList] = useState<IngredientList[]>([
     { id: "5", Ingredient: "Ingredient", Measurement: "Measurement"}
   ]);
   const [StepList, setStepList] = useState<StepList[]>([
     { id: "5", Step: "Steps"}
   ]);
-
+	function writeUserData() {
+		var i;
+		var tempsteplist = [];
+		var tempinglist = [];
+		var tempmeaslist = [];
+		for (i = 0; i < IngredientList.length; i++) {
+		  if (IngredientList[i].Ingredient === ""){
+			  errorMessage = "Ingredient fields cannot be blank";
+			  return;
+		  }
+		  tempinglist.push(IngredientList[i].Ingredient);
+		  if (IngredientList[i].Measurement === ""){
+			  errorMessage = "Measurement fields cannot be blank";
+			  return;
+		  }
+		  tempmeaslist.push(IngredientList[i].Measurement);
+		} 
+		for (i = 0; i < StepList.length; i++) {
+		  if (StepList[i].Step === ""){
+			  errorMessage = "Step fields cannot be blank";
+			  return;
+		  }
+		  tempsteplist.push(StepList[i].Step);
+		} 
+		if (title === ""){
+		  errorMessage = "Title field cannot be blank";
+		  return;
+		}
+		if (description === ""){
+		  errorMessage = "Description field cannot be blank";
+		  return;
+		}
+		console.log(title);
+		console.log(description);
+		console.log(JSON.stringify(StepList));
+		console.log(JSON.stringify(IngredientList));
+		firebase.database().ref('recipes/' + generate()).set({
+		  title: title,
+		  description: description,
+		  steps: tempsteplist,
+		  ingredients: tempinglist,
+		  measurements: tempmeaslist
+		});
+	}
   return (
     <div style={{ textAlign: "center" }}>
 		<TextForm />
@@ -140,11 +184,20 @@ const UploadRecipe = () => {
           </div>
         );
       })}
-      <div>{JSON.stringify(StepList, null, 2)}</div>
-
+      <div>
+			<button onClick={writeUserData}>
+          upload recipe
+            </button>
+		</div>
+	  <div>{JSON.stringify(StepList, null, 2)}</div>
+	
+      <div>
+		<p>{errorMessage}</p>
+	  </div>
     </div>
   );
 
 };
+
 
 export default UploadRecipe;

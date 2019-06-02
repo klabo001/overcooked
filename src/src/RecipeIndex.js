2ed175428e1262c7dc911e807a5bac5bc1ab629e
Ji Hoon Choi <jchoi071@ucr.edu>
2019-06-02 01:19:28 -0700

    Added working bookmark buttons and database read/write functionality

--- src/src/RecipeIndex.js
+++ src/src/RecipeIndex.js
@@ -6,15 +6,45 @@ import UploadRecipe from './RecipeUpload.tsx';
 import updateApp from './index.js';
 import Bookmark from './bookmark.js';
 var listings = [];
+var uid;
+var i;
+var bmFound;
+
+function GetBookmarks(bm, d)
+{
+	bmFound = false;
+	var values = Object.values(bm.val().bookmarks);
+	var keys = Object.keys(bm.val().bookmarks);
+	for (i = 0; i < values.length; i++)
+	{
+		if (values[i] == d.key)
+		{
+			bmFound = true;
+		}
+	}
+}
 
 function RecipeIndex() {
-	var ref = firebase.database().ref("recipes");
-	ref.orderByValue().on("value", function(snapshot) {
+	firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    uid = user.uid
+  } else {
+    // No user is signed in.
+  }
+});
+
+	var recipe = firebase.database().ref("recipes");
+
+	recipe.orderByValue().on("value", function(snapshot) {
 	  snapshot.forEach(function(data) {
+			var bookmark = firebase.database().ref("accounts/" + uid);
+			bookmark.on("value", function(snapshot)
+			{
+				GetBookmarks(snapshot, data);
+
 			var ing = data.val().ingredients;
 		  var meas = data.val().measurements;
 		  var stps = data.val().steps;
-		  var i;
 			var ingList = [];
 			var stepsList = [];
 
@@ -29,22 +59,23 @@ function RecipeIndex() {
 		  var listing =
 			(
 			<div>
-			<Bookmark />
 		  <h1>{ data.val().title }</h1>
+			<Bookmark found={ bmFound } uid={ uid } rid={ data } bookmarks={ firebase.database().ref("accounts/" + uid).child("bookmarks") } keys={ Object.keys(snapshot.val().bookmarks) } values={ Object.values(snapshot.val().bookmarks) }/>
 		  <h2>{ data.val().description }</h2>
 		  <h2>Recipe by: { data.val().user }</h2>
-		  <ul>
+			<h3>Ingredients</h3>
+		  <ol>
 			{ingList}
-		  </ul>
+		  </ol>
 		  <h3>Steps</h3>
-			<ul>
+			<ol>
 			{stepsList}
-		  </ul>
+		  </ol>
 			</div>
 			)
-		console.log(listing);
 		listings.push(listing);
 	  });
+	});
 	});
 }
 RecipeIndex();
--- src/src/bookmark.js
+++ src/src/bookmark.js
@@ -1,51 +1,71 @@
 import React from 'react';
-
+import firebase from './firebase.js';
 class Bookmark extends React.Component {
+
 
   constructor(props){
 
     super(props);
-    this.state ={
-      bookmark: 124,
-      updated: false
+
+    if (this.props.found)
+    {
+      this.state ={
+        updated: this.props.found,
+        text: "Bookmarked!"
+      }
+    }
+    else
+    {
+      this.state ={
+        updated: this.props.found,
+        text: "Bookmark"
+      }
     }
     this.updateBookmark = this.updateBookmark.bind(this);
   }
 
   updateBookmark() {
-
     if(!this.state.updated) {
+      var newKey = this.props.bookmarks.push(this.props.rid.key);
+      this.props.keys.push(newKey.key);
+      this.props.values.push(this.props.rid.key);
       this.setState((prevState, props) => {
         return {
-          bookmark: prevState.bookmark + 1,
-          updated: true
+          updated: true,
+          text: "Bookmarked!"
+
         };
       });
     } else {
+      for (var i = 0; i < this.props.values.length; i++)
+    	{
+        var k = this.props.keys[i];
+    		if (this.props.values[i] == this.props.rid.key)
+    		{
+          //console.log(this.props.keys[i] + " " + this.props.uid);
+          var path = "accounts/" + [this.props.uid] + "/bookmarks/" + [this.props.keys[i]];
+          console.log(path);
+    			firebase.database().ref(path).remove();
+    		}
+    	}
 
       this.setState((prevState, props) => {
         return {
-          bookmark: prevState.bookmark - 1,
-          updated: false
+          updated: false,
+          text: "Bookmark"
         };
       });
     }
-
-
+    //window.location.reload();
   }
 
   render(){
-
     return(
-      <div>
-        <p onClick={this.updateBookmark}>Bookmark</p>
-        <p>{this.state.bookmark}</p>
-      </div>
+      <button>
+        <p onClick={this.updateBookmark}>{ this.state.text }</p>
+      </button>
     );
-
   }
-
-
 }
 
 export default Bookmark;

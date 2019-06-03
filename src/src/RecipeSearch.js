d24f92d4c5343971c662278d9822f8af761bc4b0
jrodr087 <jrodr087@ucr.edu>
2019-06-02 17:14:30 -0700

    adds bookmarks to search results as well as requiring a sign in before searching

--- src/src/RecipeSearch.js
+++ src/src/RecipeSearch.js
@@ -2,14 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 import firebase from './firebase.js';
-import UploadRecipe from './RecipeUpload.tsx';
+//import UploadRecipe from './RecipeUpload.tsx';
 import updateApp from './index.js';
+import Bookmark from './bookmark.js';
 var listings = [];
 var searchval = "";
 var error = "";
 var spaceregex = / +/;
 var quoteregex = /"([^"]*)"/g;
 var curpage;
+var bmFound;
+var uid;
+firebase.auth().onAuthStateChanged(function(user) {
+	  if (user) {
+		uid = user.uid;
+	  }
+	  else{
+		  uid = 0;
+	  }
+});
+function GetBookmarks(bm, d)
+{
+	var i;
+	bmFound = false;
+	var values = Object.values(bm.val().bookmarks);
+	var keys = Object.keys(bm.val().bookmarks);
+	for (i = 0; i < values.length; i++)
+	{
+		if (values[i] == d.key)
+		{
+			bmFound = true;
+		}
+	}
+}
 function FoundInput(data, vals){
 	var foundflag = false;
 	var k;
@@ -43,7 +68,7 @@ function handleReturnToSearchInput(){
 	curpage = <InputSearchPage/>;
 	updateApp();
 }
-function RecipeSearch() {
+function ParseSearchInput(){
 	listings.length = 0;
 	var quotes = searchval.match(quoteregex); // gets every search term between quotes
 	var searchValues = [];
@@ -61,40 +86,71 @@ function RecipeSearch() {
 		searchValues = searchValues.concat(quotes);
 	}
 	console.log(searchValues);
-	var ref = firebase.database().ref("recipes");
-	ref.orderByValue().on("value", function(snapshot) {
-	  snapshot.forEach(function(data) {
-		  var testflag = FoundInput(data, searchValues);
-		  if (testflag == false){
-		  }
-		  else{
-			  var listing = "<div>";
-			  listing = listing + "<h1>" + data.val().title + "</h1>";
-			  listing = listing + "<h2>" + data.val().description + "</h2>";
-			  listing = listing + "<h2>Recipe by: " + data.val().user + "</h2>";
-			  var ing = data.val().ingredients;
-			  var meas = data.val().measurements;
-			  var stps = data.val().steps;
-			  var i;
-			  listing = listing + "<ul>"
-			  for (i = 0; i < ing.length; i++){
-				  listing = listing + "<li>" + ing[i] + ", " + meas[i] + "</li>";
+	return searchValues;
+}
+function RecipeSearch() {
+	
+	var searchValues = ParseSearchInput();
+	//var ref = firebase.database().ref("recipes");
+  if (uid) {
+	var i;
+	listings.length = 0;
+		var recipe = firebase.database().ref("recipes");
+		recipe.orderByValue().on("value", function(snapshot) {
+		  snapshot.forEach(function(data) {
+			  var testflag = FoundInput(data, searchValues);
+			  if (testflag == false){
 			  }
-			  listing = listing + "</ul>"
-			  listing = listing + "<h3>Steps</h4>";
-			  listing = listing + "<ul>"
-			  for (i = 0; i < stps.length; i++){
-				  listing = listing + "<li>" + stps[i] + "</li>";
-			  }
-			  listing = listing + "</ul>"
-			listing = listing + "</div>";
+			  else{
+				var bookmark = firebase.database().ref("accounts/" + uid);
+				bookmark.on("value", function(snapshot)
+				{
+					GetBookmarks(snapshot, data);
+					var ing = data.val().ingredients;
+				  var meas = data.val().measurements;
+				  var stps = data.val().steps;
+					var ingList = [];
+					var stepsList = [];
+
+				  for (i = 0; i < ing.length; i++){
+					  //listing = listing + "<li>" + ing[i] + ", " + meas[i] + "</li>";
+						ingList.push(<li>{ing[i]}, {meas[i]}</li>);
+				  }
+					for (i = 0; i < stps.length; i++){
+					  //listing = listing + "<li>" + stps[i] + "</li>";
+						stepsList.push(<li>{stps[i]}</li>);
+				  }
+				  var listing =
+					(
+					<div>
+				  <h1>{ data.val().title }</h1>
+					<Bookmark found={ bmFound } uid={ uid } rid={ data } bookmarks={ firebase.database().ref("accounts/" + uid).child("bookmarks") } keys={ Object.keys(snapshot.val().bookmarks) } values={ Object.values(snapshot.val().bookmarks) }/>
+				  <h2>{ data.val().description }</h2>
+				  <h2>Recipe by: { data.val().user }</h2>
+					<h3>Ingredients</h3>
+				  <ol>
+					{ingList}
+				  </ol>
+				  <h3>Steps</h3>
+					<ol>
+					{stepsList}
+				  </ol>
+					</div>
+					)
+				listings.push(listing);
+			  });
+			}
+			});
+		});
+		/*if (listings.length == 0){
+			var listing = "<div><h1><p>No results found</p></h1></div>";
 			listings.push(listing);
-		  }
-	  });
-	});
-	if (listings.length == 0){
-		var listing = "<div><h1><p>No results found</p></h1></div>";
+		}*/
+	} else {
+		listings.length = 0;
+		var listing = <div><h1><p>Login before searching.</p></h1></div>;
 		listings.push(listing);
+	// No user is signed in.
 	}
 }
 
@@ -108,11 +164,10 @@ class SearchPage extends React.Component {
 	<div>
 		<h4>
 			<p>Return to search?.</p>
-			<input type="submit" value="search" onClick={handleReturnToSearchInput}/>
+			<input type="submit" value="Return" onClick={handleReturnToSearchInput}/>
 		</h4>
 	</div>
-	<div dangerouslySetInnerHTML={{__html: listings}}>
-	</div>
+	<div>{ listings }</div>
 	</div>
 	return page;
 	}
@@ -161,6 +216,7 @@ class InputSearchPage extends React.Component {
 	}
 }
 curpage = <InputSearchPage/>;
+
 function Search(){
 	return curpage;
 }
